06bea2e8c8a34ed0bf7e0e2353728b42f57d532d
Hans <alvarohansorellana@gmail.com>
2024-10-29 13:25:43 +0100

    first js logic

---
+++ script.js
@@ -0,0 +1,11 @@
+// URL de la API
+const booksApi = "";
+
+// Elementos manipulables en el DOM
+const bookListsContainer = document.getElementById('book-lists');
+const loader = document.getElementById('loader');
+
+// Función asíncrona para obtener las listas de la API
+async function fetchBooks() {
+    loader.style.display =
+}
